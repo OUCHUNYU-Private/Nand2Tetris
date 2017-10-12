99570059cceb7e422466cc665aefbf92360200ad
Chunyu Ou <Chunyu@UnifyIDs-MacBook-Pro-2.local>
2017-10-11 22:42:39 -0700

    Finish fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,61 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+		// create a variable to hold total pixle count
+		// screen is 16 by 32 size
+		@8191
+		D=A
+		@total_pixles
+		M=D
+
+	(INFINITE_LOOP)
+		// temp counter for screen
+		@pixle_counter
+		M=0
+
+		// assign screen address to a pointer 
+		@SCREEN
+		D=A
+		@screen_address
+		M=D
+
+		// set default fill value to 0 which is white
+		@fill_value
+		M=0
+		
+		@KBD  // accesss keyboard input
+		D=M
+		
+		// set the value to black if there is any keyboard input
+		@BLACKEN
+		D;JGT
+
+		@FILL_LOOP
+		0;JMP
+		
+	(BLACKEN)
+		@fill_value
+		M=-1
+
+	(FILL_LOOP)
+		// access the fill value and use it on the cuurent screen address
+		@fill_value
+		D=M
+		@screen_address
+		A=M
+		M=D
+		
+		// if total - counter <= 0 jump back to INFINITE_LOOP
+		@total_pixles
+		D=M
+		@pixle_counter
+		D=D-M
+		@INFINITE_LOOP
+		D;JLE
+		// else counter ++ and jump back to FILL_LOOP
+		@pixle_counter
+		M=M+1
+		@screen_address
+		M=M+1
+		@FILL_LOOP
+		0;JMP
